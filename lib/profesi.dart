6410889edca60dfa3cbde5784cf8d2074c4e6914
maurin <maurinrin16@gmail.com>
2026-09-04 08:40:39 +0700

    update style

--- lib/profesi.dart
+++ lib/profesi.dart
@@ -32,7 +32,7 @@ const Profesi({ Key? key }) : super(key: key);
                     padding: EdgeInsets.fromLTRB(8, 6, 8, 2), 
                     child: Text(
                       'Guru PPPK dialihkan ke pemerintah pusat - Apa maknanya?',
-                      style: TextStyle(fontSize: 13, color: Color.fromARGB(255, 32, 29, 29),), 
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,),
                     ),
                   ),
                   const Padding(
